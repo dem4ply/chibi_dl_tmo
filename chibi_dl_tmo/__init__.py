4d6ae9623a666f14e7df7c30879c5343f184fab8
dem4ply <dem4ply@gmail.com>
2024-03-04 17:44:20 -0600

    Bump version: 0.0.2 → 0.0.3

--- chibi_dl_tmo/__init__.py
+++ chibi_dl_tmo/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = """dem4ply"""
 __email__ = 'dem4ply@gmail.com'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .site import TMO_fans
 
--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     name='chibi_dl_tmo',
     packages=find_packages(include=['chibi_dl_tmo', 'chibi_dl_tmo.*']),
     url='https://github.com/dem4ply/chibi_dl_tmo',
-    version='0.0.2',
+    version='0.0.3',
     zip_safe=False,
 )
